5a17987043af01601f9dc336bcc1a0444fe92220
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-05-04 18:37:50 +0200

    Reafactor - remove console.logs

--- server.js
+++ server.js
@@ -24,28 +24,19 @@ const server = app.listen(port, () => console.log(`Example app listening at http
 const io = socket(server);
 
 io.on('connection', (socket) => {
-  console.log('New client! Its id – ' + socket.id);
-
   socket.on('message', (message) => {
-    console.log('Oh, I\'ve got something from ' + socket.id);
     messages.push(message);
     socket.broadcast.emit('message', message);
   });
 
   socket.on('login', (user) => {
-    console.log(`User ${socket.id} logged in`);
     users.push({ name: user, id: socket.id });
-    console.log(users);
     socket.broadcast.emit('message', { author: 'Chat Bot', content: `${user} has joined the conversation!` });
   });
 
   socket.on('disconnect', () => {
-    console.log('Oh, socket ' + socket.id + ' has left');
     const user = users.find(user => user.id === socket.id);
     socket.broadcast.emit('message', { author: 'Chat Bot', content: `${user.name} has left the conversation... :(` });
     users.splice(users.indexOf(user), 1);
-    console.log(users);
   });
-
-  console.log('I\'ve added a listener on message event \n');
 });
